117e535b3eb27fc0273d0f327bfa909e5c4ddba9
hanminghao <hmh950728@126.com>
2019-01-30 13:57:37 -0800

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,11 +60,13 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "IonicaBizau");
-    
+    // GitHubCalendar("#github-graph", "MHhmMH");
+    GitHubCalendar("#github-graph-MHhmMH", "MHhmMH");
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "MHhmMH", selector: "#ghfeed_selector"});
+    // GitHubActivity.feed({ username: "MHhmMH", selector: "#ghfeed"});
+
 
 
 });
